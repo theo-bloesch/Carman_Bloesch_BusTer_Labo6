2293f3b88dec521d500377215c11b584f9972d70
Theo Bloesch <91142911+theo-bloesch@users.noreply.github.com>
2023-05-15 18:34:34 +0200

    conveyerOk

--- Carman_Bloesch_BusTer_Labo6.cpp
+++ Carman_Bloesch_BusTer_Labo6.cpp
@@ -3,6 +3,7 @@
 #include <winsock2.h>
 #include <WS2tcpip.h>
 #include <iostream>
+#include <string>
 #define DATA_SIZE 1024
 #define ADDRESS_TEXT_SIZE 128
 
@@ -49,10 +50,15 @@ enum MessageType {
 	REP_PRESENCE = 0x61
 };
 
-const char* SetMessage(MessageType messageType, INT8 messageNum,INT8 dataLength,INT8* data)
+void SetMessage(char* Message, MessageType messageType, char messageNum, char dataLength, char* data)
 {
+	//Message = (char*)malloc((dataLength+3) * sizeof(char));
+	/*if (Message == NULL)
+	{
+		std::cout << "Error" << std::endl;
+		return;
+	}*/
 	
-	char Message[104];
 	Message[0] = messageType;
 	Message[1] = messageNum;
 	Message[2] = dataLength;
@@ -60,21 +66,21 @@ const char* SetMessage(MessageType messageType, INT8 messageNum,INT8 dataLength,
 	{
 		Message[3 + i] = data[i];
 	}
-	INT8 controlSum = 0;
-	for (int i = 0; i < dataLength; i++)
+	char controlSum = 0;
+	for (int i = 0; i < dataLength+3 ; i++)
 	{
 		controlSum += Message[i];
 	}
 	Message[3 + dataLength] = controlSum;
-	return Message;
+	Message[3 + dataLength+1] = '\0';
 	
 }
 
-char ChooseUserMenu(){
+char ChooseUserMenu() {
 	char chosenMenu = '0';
 	bool firstRun = true;
 	do {
-		if(firstRun){
+		if (firstRun) {
 			std::cout << "Assemblage de processeurs =====================" <<
 				"1. Commandes manuelles\n" <<
 				"2. Pilotage automatise\n" <<
@@ -84,7 +90,7 @@ char ChooseUserMenu(){
 				"9. Quitter le programme\n" <<
 				"Choix > ";
 		}
-		else{
+		else {
 			std::cout << " Try again\n" << "Choix > ";
 		}
 		std::cin >> chosenMenu;
@@ -93,67 +99,55 @@ char ChooseUserMenu(){
 	return chosenMenu;
 }
 
-//void ManagerUserMenu(){
-//	char chosenMenu;
-//    do {
-//		chosenMenu = ChooseUserMenu();
-//		switch (chosenMenu) {
-//		case '1':  ManagerManuelMenu(); break;
-//		case '2': break;
-//		case '3': break;
-//		case '4': break;
-//		case '5': break;
-//		case '9': break;
-//		}
-//	} while (chosenMenu != 9);
-//}
-
-char ChooseMenuManuel(){
+char ChooseMenuManuel() {
 	char chosenMenu = '0';
 	bool firstRun = true;
 	do {
 		if (firstRun) {
-		std::cout << "Commandes manuelles ------------------------" <<
-			"1. Allumer le convoyeur\n" <<
-			"2. Arreter le convoyeur\n" <<
-			"3. Afficher l'etat du capteur de palette\n" <<
-			"4. Activer le vacuum\n" <<
-			"5. Desactiver le vacuum\n" <<
-			"6. Afficher l'etat presence piece\n" <<
-			"7. Deplacer le robot en position Home\n" <<
-			"8. Deplacer le robot en position Pick\n" <<
-			"9. Deplacer le robot en position Place\n" <<
-			"10. Afficher si le robot est en mouvement\n" <<
-			"99. Quitter les fonctions manuelles\n" <<
-			"Choix > ";
-			}
-			else{
-				std::cout << " Try again\n" << "Choix > ";
-			}
-			std::cin >> chosenMenu;
-			firstRun = false;
-		} while (!(chosenMenu > '0' && chosenMenu <= '10' || chosenMenu == '99'));
-		return chosenMenu;
-	}
+			std::cout << "Commandes manuelles ------------------------" <<
+				"1. Allumer le convoyeur\n" <<
+				"2. Arreter le convoyeur\n" <<
+				"3. Afficher l'etat du capteur de palette\n" <<
+				"4. Activer le vacuum\n" <<
+				"5. Desactiver le vacuum\n" <<
+				"6. Afficher l'etat presence piece\n" <<
+				"7. Deplacer le robot en position Home\n" <<
+				"8. Deplacer le robot en position Pick\n" <<
+				"9. Deplacer le robot en position Place\n" <<
+				"10. Afficher si le robot est en mouvement\n" <<
+				"99. Quitter les fonctions manuelles\n" <<
+				"Choix > ";
+		}
+		else {
+			std::cout << " Try again\n" << "Choix > ";
+		}
+		std::cin >> chosenMenu;
+		firstRun = false;
+	} while (!(chosenMenu > '0' && chosenMenu <= '10' || chosenMenu == '99'));
+	return chosenMenu;
+}
+
+
+
 
 void ManagerManuelMenu() {
 	char chosenMenu;
 	do {
 		chosenMenu = ChooseMenuManuel();
 		switch (chosenMenu) {
-		case '1': break;
-		case '2': break;
-		case '3': break;
-		case '4': break;
-		case '5': break;
-		case '6': break;
-		case '7': break;
-		case '8': break;
-		case '9': break;
-		case '10': break;
-		case '99': break;
+		case '1': break; // convoyeur on
+		case '2': break; // convoyeur off
+		case '3': break; // show presence pallette
+		case '4': break; // vacuum On
+		case '5': break; // vacuum off
+		case '6': break; // show presence piece
+		case '7': break; // Home
+		case '8': break; // Pick
+		case '9': break; // Place
+		case '10': break; // show mouvement
+		case '99': std::cout << "Quitte le menu des fonctions manuelles" << std::endl; break;
 		}
-	} while (chosenMenu != 99);
+	} while (chosenMenu != '99');
 }
 
 int wait_received(SOCKET socket, long timeout_milliseconds)
@@ -166,18 +160,19 @@ int wait_received(SOCKET socket, long timeout_milliseconds)
 	FD_ZERO(&fd_set_receive);
 	FD_SET(socket, &fd_set_receive);
 	status = select(0, &fd_set_receive, NULL, NULL, &timeout);
+
 	if (status == SOCKET_ERROR)
 	{
 		printf("erreur d'utilisation de socket.\n");
-		
+
 	}
 	return status > 0;
 }
 
-void AutoDetection()
-{
-	
-}
+
+
+
+
 
 int main()
 {
@@ -187,7 +182,7 @@ int main()
 	struct sockaddr_in server_address;
 	struct sockaddr_in response_source_address;
 	int response_source_address_length;
-	char address_text[ADDRESS_TEXT_SIZE] = {127,0,0,1};
+	//char address_text[ADDRESS_TEXT_SIZE] = { 127,0,0,1 };
 	char data_to_send[DATA_SIZE];
 	int quit;
 	char received_data[DATA_SIZE];
@@ -196,16 +191,11 @@ int main()
 	WSAStartup(MAKEWORD(2, 2), &info);
 	// Créer le socket UDP
 	socket_client = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-	// Saisie de l'adresse IP V4
-	/*printf("Adresse IPV4:");
-	fgets(address_text, sizeof address_text, stdin);*/
-	address_text[strlen(address_text) - 1] = '\0';
-	
 	// Preparer la structure avec l'adresse et le port de destination
 	memset(&server_address, 0, sizeof server_address);
 	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(PORT_NUMBER); // Port network byte order
-	inet_pton(AF_INET, address_text, &server_address.sin_addr);
+	inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
 	//Autoriser la diffusion restreinte
 	int value = 1;
 	setsockopt(socket_client, SOL_SOCKET, SO_BROADCAST, (char*)&value, sizeof(value));
@@ -221,15 +211,29 @@ int main()
 		if (!quit)
 		{
 			// Envoyer les données
-			INT8 data = 1;
+			
+			char Message[105] = {0};
+			char data = 1;
+			//char* Message = NULL;
+			char dataLength = 1;
+			//Message = (char*)malloc((dataLength + 3) * sizeof(char));
+			
+			SetMessage(Message, COM_CONVEYOR, 1, dataLength, &data);
+			std::cout << std::to_string(Message[0]) << std::endl;
+			std::cout << std::to_string(Message[1]) << std::endl;
+			std::cout << std::to_string(Message[2]) << std::endl;
+			std::cout << std::to_string(Message[3]) << std::endl;
+			std::cout << std::to_string(Message[4]) << std::endl;
+			std::cout << strlen(Message) << std::endl;
+			//Attention strlen va jusqu'au /0
 			status = sendto(socket_client,
-				SetMessage(COM_CONVEYOR,10,1,&data), strlen(data_to_send), 0,
+				Message, strlen(Message), 0,
 				(struct sockaddr*)&server_address,
 				sizeof server_address);
-			
+
 			// Recevoir les données et l'adresse de l'émetteur
 			long timeout_milliseconds = 1000;
-			if (wait_received(socket_client, timeout_milliseconds)>0)
+			if (wait_received(socket_client, timeout_milliseconds) > 0)
 			{
 				response_source_address_length = sizeof response_source_address;
 				received_data_size = recvfrom(socket_client,
@@ -242,6 +246,28 @@ int main()
 			{
 				received_data[received_data_size] = '\0';
 				printf("Reponse recue:%s\n", received_data);
+				for (int i = 0; i < received_data_size-1; i++)
+				{
+					std::cout << received_data[i];
+				}
+				std::cout<<std::endl;
+				/*std::cout << received_data[0] << std::endl;
+				std::cout << received_data[1]<< std::endl;
+				std::cout << received_data[2] << std::endl;
+				std::cout << (received_data[3]) << std::endl;
+				std::cout << (received_data[4]) << std::endl;
+				std::cout << (received_data[5]) << std::endl;
+				std::cout << (received_data[6]) << std::endl;
+				std::cout << (received_data[7]) << std::endl;
+				std::cout << (received_data[8]) << std::endl;
+				std::cout << (received_data[9]) << std::endl;
+				std::cout << (received_data[10]) << std::endl;
+				std::cout << (received_data[11]) << std::endl;
+				std::cout << (received_data[12]) << std::endl;
+				std::cout << (received_data[13]) << std::endl;
+				std::cout << (received_data[14]) << std::endl;
+				std::cout << (received_data[15]) << std::endl;*/
+				
 			}
 			else
 				printf("Erreur de reception\n");
